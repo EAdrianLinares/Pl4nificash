35ad09260753a3d013b8dc1eea00d70c5e58dc2b
nukak.dev <nukak.dev@gmail.com>
2026-08-07 14:59:50 -0500

    conexion local ok

--- planificash-backend/src/main.ts
+++ planificash-backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
       origin: [
-        'http://localhost:5175',
+        'http://localhost:5173',
         'https://pl4nificash.vercel.app',
         'https://pl4nificash-1.onrender.com', // develop
         'https://pl4nificash.onrender.com',   // main
